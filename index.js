ecbdff9aab5868204af42d86701cae576337be4d
emreeken183425 <emreeken1834@gmail.com>
2022-07-02 19:23:38 +0300

    javascript

---
+++ index.js
@@ -0,0 +1,65 @@
+const passInput=document.querySelector(".input-group input");
+const toggleIcon=document.querySelector(".input-group .toggle");
+const ripple=document.querySelector(".input-group .ripple");
+const percentBar=document.querySelector(".strength-percent span");
+const passLabel=document.querySelector(".strength-Label");
+passInput.addEventListener("input" ,handlePassInput );
+toggleIcon.addEventListener("click",togglePassInput);
+
+function handlePassInput(e){
+    if(passInput.ariaValueMax.length===0){
+        passLabel.innerHTML="strength ";
+        addClass();
+    }else if(passInput.ariaValueMax.length<=4 ){
+        passLabel.innerHTML="weak"
+        addClass("weak");
+    }else if(passInput.ariaValueMax.length<=7 ){
+        passLabel.innerHTML="Not bad"
+        addClass("average");
+    }else{
+        passLabel.innerHTML="strong";
+        addClass("strong")
+    }
+}
+
+function addClass(className){
+    percentBar.classList.remove("weak");
+    percentBar.classList.remove("average");
+    percentBar.classList.remove("strong");
+    if(className){
+        percentBar.classList.add(className);
+    }
+}
+
+function togglePassInput(e){
+    const type =passInput.getAttribute("type");
+    if(type==="password" ){
+        passInput.setAttribute("type","text");
+        toggleIcon.innerHTML="☠ ";
+        ripple.getElementsByClassName.cssText=`
+        border-radius:4px;
+        width:100%;
+        height:100%;
+        right:0;
+        z-index:-1;
+        `;
+        passInput.getElementsByClassName.color="#000";
+        passInput.getElementsByClassName.background="transparent";
+        toggleIcon.getElementsByClassName.fontSize="27px";
+
+    }else{
+        passInput.setAttribute("type","password");
+        toggleIcon.innerHTML="🧨";
+        toggleIcon.style.fontSize="25px";
+        ripple.style.cssText=`
+        border-radius: 50%;
+  height: 35px;
+  width: 35px;
+  right: 20px;
+  z-index: 1;
+        `;
+        passInput.style.color="#fff";
+        passInput.style.background="#112d37";
+
+    }
+}
